16c8524aae05b7f12cfb93cfbfb4e8a22c3c366f
zhongqiang <zq342500@163.com>
2021-08-17 23:39:57 +0800

    实现menu组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import React from "react";
 import "./styles/index.scss";
+import Menu from "./components/Menu/menu";
+import MenuItem from "./components/Menu/menuItem";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Menu defaultIndex="0" onSelect={(index) => alert(index)}>
+        <MenuItem>0000</MenuItem>
+        <MenuItem>ssss</MenuItem>
+      </Menu>
+    </div>
+  );
 }
 
 export default App;
